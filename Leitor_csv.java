713467de5cf17fa91cfaff6b4a29d0bae17cef6c
Ana Luiza Diniz <anadiniz.ds@gmail.com>
2023-04-08 14:22:15 -0300

    Leitor CSV

---
+++ Leitor_csv.java
@@ -0,0 +1,57 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.RandomAccessFile;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Leitor_csv {
+
+    public static void leitor() throws IOException, ParseException{
+
+        File arquivoCSV = new File("/home/anadiniz/Documentos/Faculdade/AEDs 3/tp_aeds3/FilmesDatabase.csv");
+		SimpleDateFormat formato = new SimpleDateFormat("yyyy");
+
+		RandomAccessFile arq;
+	
+		try {
+				
+			Scanner leitor = new Scanner (arquivoCSV);
+		
+			String linhaarquivo =new  String();
+			leitor.nextLine();
+			int i=0;
+			while(i<20)
+				{ i++;
+				//split das linhas do csv
+				linhaarquivo=leitor.nextLine();
+				String[] valores = linhaarquivo.split(";");
+				
+				
+				float nota = Float.parseFloat(valores[3]);
+				String startdate = (valores[2]);
+				Date date = formato.parse(startdate);
+				int id = Integer.parseInt(valores[0]);
+				
+				
+				// adicionando os conteudos no objeto e e escrevendo ele em byte
+				//contador=id, valores[1]=titulo 
+				// nota= float(nota do filme ) // valores[4]=generos do filme
+				
+				Filmes filme1 = new Filmes(id,valores[1],nota,valores[4], date);
+			
+				Escrita_db.escrita(filme1);
+				//leitura do arquivo em byte no hexa.db
+				//filme filme2 = new filme();
+				//leitura(filme2);
+			}
+		}  
+		catch(FileNotFoundException e) { e.printStackTrace();
+		
+	}
+    }
+    
+}
