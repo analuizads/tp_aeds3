10c8a230f70472cb9a03dce39d852eb6707d652e
Ana Luiza Diniz <anadiniz.ds@gmail.com>
2023-05-16 23:02:13 -0300

    utilização de index

--- Escrita_db.java
+++ Escrita_db.java
@@ -22,15 +22,25 @@ public class Escrita_db {
 		 //criar  em byte o chars
 		byte[] ba;
 		ba = Filmes.toByteArray(filme1);
+		// coloca ponteiro no 0 e escreve id
 		arq.seek(0);
 		//Criando cabeçalho
 		arq.writeInt(filme1.getId());
+		// escreve vetor de bytes
+		// coloca variavel com o valor do ultimo byte do arquivo
 		ponteiro = arq.length();
+		// preenche o indice
+		Index indice = new Index(filme1.getId(), ponteiro);
+		Index.escritorIndex(indice);
+		// manda o ponteiro para o final do arquivo e escrevendo proximo registro
 		arq.seek(ponteiro);
 		arq.writeByte(lapide);
+		// escreve tamanho do registro
 		arq.writeInt(ba.length);
+		//ecreve vetor de bytes no arquivo hexa.db
 		arq.write(ba);
 		System.out.println(filme1);
+		arq.close();
 	}  
 
 }
